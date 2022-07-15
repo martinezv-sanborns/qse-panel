e0ed8d1b2a728f7054ce1deb302ed93383370bbc
Jorge Eduardo Juarez Ceron <juarezj@sanborns.com.mx>
2022-07-15 17:01:39 -0500

    ccccc

--- src/app/pages/usuarios/usuarios.page.ts
+++ src/app/pages/usuarios/usuarios.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuarioService } from '../../services/usuario.service';
+import { UsuarioResponse } from '../../models/response/usuario.model';
 
 @Component({
   selector: 'app-usuarios',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsuariosPage implements OnInit {
 
-  constructor() { }
+  Usuarios: UsuarioResponse [] = [];
+
+  cargando:false;
+
+  constructor(private usuarioService: UsuarioService) {
+
+
+   }
 
   ngOnInit() {
+  
+  this.usuarioService.ObtenerListado(1,10000).subscribe(exito=>{
+
+   
+    this.Usuarios= exito.dtoResult;
+
+    console.log(exito);
+  });
+  
   }
+
+
+  Activar(usuarioId:string){
+
+
+  }
+
+
+  Desbloquear(usuario:string){}
 
 }
